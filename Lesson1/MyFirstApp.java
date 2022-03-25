9a68d76bef6958720ada947fb274d9e0c36e640b
TimaSerp <101899267+TimaSerp@users.noreply.github.com>
2022-03-25 13:46:21 +0300

    Проверено

--- Lesson1/MyFirstApp.java
+++ Lesson1/MyFirstApp.java
@@ -1,6 +1,4 @@
-import java.util.Scanner;
-
-public class MyFirstApp {
+public class MyFirstApp { 
     public static void main(String[] args) {
         System.out.print("Написано однажды,");
         System.out.println(" работает везде!");
