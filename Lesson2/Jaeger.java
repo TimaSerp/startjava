4bdf92b477bfbf5fa80de35e5640369181bbd297
TimaSerp <timaserp@gmail.com>
2022-03-27 12:01:17 +0300

    добавил дз по Jaeger

---
+++ Lesson2/Jaeger.java
@@ -0,0 +1,72 @@
+public class Jaeger {
+    private String modelName;
+    private String mark;
+    private String origin;
+    private String fightStyle;
+    private double height;
+    private double weight;
+
+    public String getModelName() {
+        return modelName;
+    }
+
+    public void setModelName(String modelName) {
+        this.modelName = modelName;
+    }
+
+    public String getMark() {
+        return mark;
+    }
+
+    public void setMark(String mark) {
+        this.mark = mark;
+    }
+
+    public String getOrigin() {
+        return origin;
+    }
+
+    public void setOrigin(String origin) {
+        this.origin = origin;
+    }
+
+    public String getFightStyle() {
+        return fightStyle;
+    }
+
+    public void setFightStyle(String fightStyle) {
+        this.fightStyle = fightStyle;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    boolean drift() {
+        return true;
+    }
+
+    String move() {
+        return "move";
+    }
+
+    String scanKaiju() {
+        return "scanning";
+    }
+
+    String usePlasmaCaster() {
+        return "shooting";
+    }
+}
---
+++ Lesson2/JaegerTest.java
@@ -0,0 +1,60 @@
+import java.util.Scanner;
+
+public class JaegerTest {
+    public static void main(String[] args) {
+        Jaeger gipsyAvenger = new Jaeger();
+        Jaeger gipsyDanger = new Jaeger();
+        Scanner console = new Scanner(System.in);
+        gipsyAvenger.setModelName("Gipsy Avenger");
+        gipsyDanger.setModelName("Gipsy Danger");
+        gipsyAvenger.setMark("Mark-6");
+        gipsyDanger.setMark("Mark-3");
+        gipsyAvenger.setOrigin("Unknown");
+        gipsyDanger.setOrigin("USA");
+        gipsyAvenger.setFightStyle("Street Fighter");
+        gipsyDanger.setFightStyle("Street Fighter");
+        gipsyAvenger.setHeight(81.77);
+        gipsyDanger.setHeight(79.25);
+        gipsyAvenger.setWeight(2004);
+        gipsyDanger.setWeight(1980);
+        System.out.println(gipsyAvenger.getModelName());
+        System.out.println("Mark: " + gipsyAvenger.getMark());
+        System.out.println("Origin: " + gipsyAvenger.getOrigin());
+        System.out.println("Fight style: " + gipsyAvenger.getFightStyle());
+        System.out.println("Height: " + gipsyAvenger.getHeight());
+        System.out.println("Weight: " + gipsyAvenger.getWeight());
+        System.out.println();
+        System.out.println(gipsyDanger.getModelName());
+        System.out.println("Mark: " + gipsyDanger.getMark());
+        System.out.println("Origin: " + gipsyDanger.getOrigin());
+        System.out.println("Fight style: " + gipsyDanger.getFightStyle());
+        System.out.println("Height: " + gipsyDanger.getHeight());
+        System.out.println("Weight: " + gipsyDanger.getWeight());
+
+        System.out.println("drift [on/off]");
+        String choice = console.nextLine();
+        if (choice.equals("on")) {
+            System.out.println("drift " + gipsyAvenger.drift());
+        } else {
+            System.out.println("drift " + !gipsyAvenger.drift());
+        }
+        System.out.println("What should I do? [move, scan Kaiju, use Plasma Caster]");
+        choice = console.nextLine();
+        String status = "neutral";
+        switch (choice) {
+            case "move":
+                status = gipsyAvenger.move();
+                break;
+            case "scanKaiju":
+                status = gipsyAvenger.scanKaiju();
+                break;
+            case "usePlasmaCaster":
+                status = gipsyAvenger.usePlasmaCaster();
+                break;
+            default:
+                System.out.println("Illegal status");
+                break;
+        }
+        System.out.println("Status: " + status);
+    }
+}
