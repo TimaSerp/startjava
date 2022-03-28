d8a813a469dc4be40a63c55d0ed5c8578be513b4
TimaSerp <timaserp@gmail.com>
2022-03-28 21:23:44 +0300

    убрал лишние пробелы

--- Lesson2/GuessNumber.java
+++ Lesson2/GuessNumber.java
@@ -8,8 +8,8 @@ public class GuessNumber {
     private Player player2;
 
     public GuessNumber(Player player1, Player player2) {
-            this.player1 = player1;
-            this.player2 = player2;
+        this.player1 = player1;
+        this.player2 = player2;
     }
 
     public void guessNumber() {
