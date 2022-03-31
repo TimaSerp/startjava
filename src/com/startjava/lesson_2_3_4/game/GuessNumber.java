6a7ae1fe767d1d8069a907e96af5aa78687a94aa
TimaSerp <timaserp@gmail.com>
2022-03-31 17:02:35 +0300

    загрузил дз по игре угадай число

--- src/com/startjava/lesson_2_3_4/game/GuessNumber.java
+++ src/com/startjava/lesson_2_3_4/game/GuessNumber.java
@@ -4,43 +4,70 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class GuessNumber {
+    private Scanner console = new Scanner(System.in);
     private int randomNum;
     private int playerNum;
     private Player player1;
     private Player player2;
+    private boolean isStop;
+    private int i;
 
     public GuessNumber(Player player1, Player player2) {
         this.player1 = player1;
         this.player2 = player2;
     }
 
+    public void checkNum() {
+        isStop = false;
+        if(playerNum > randomNum) {
+            System.out.println("Данное число больше того, что загадал компьютер");
+        } else if(playerNum < randomNum) {
+            System.out.println("Данное число меньше того, что загадал компьютер");
+        } else {
+            isStop = true;
+        }
+    }
+
+    public void checkWinPlayer1() {
+        System.out.println("\nИгрок " + player1.getName() + " делает свой ход.");
+        playerNum = console.nextInt();
+        checkNum();
+        player1.setNumbers1(playerNum, i);
+        if (isStop) {
+            System.out.print("\nИгрок " + player1.getName() + " угадал число " + randomNum);
+            System.out.println(" с " + i + "-й попытки");
+        } else if (i == 10) {
+            System.out.println("У игрока " + player1.getName() + " закончились попытки");
+        }
+    }
+
+    public void checkWinPlayer2() {
+        System.out.println("\nИгрок " + player2.getName() + " делает свой ход.");
+        playerNum = console.nextInt();
+        checkNum();
+        player2.setNumbers2(playerNum, i);
+        if (isStop) {
+            System.out.print("\nИгрок " + player2.getName() + " угадал число " + randomNum);
+            System.out.println(" с " + (i + 1) + "-й попытки");
+        } else if (i == 10) {
+            System.out.println("У игрока " + player2.getName() + " закончились попытки");
+        }
+    }
+
     public void guessNumber() {
-        Scanner console = new Scanner(System.in);
         Random random = new Random();
         randomNum = random.nextInt(100) + 1;
         System.out.println("В игре участвуют " + player1.getName() + " и "+ player2.getName());
-        for(;;) {
-            System.out.println("\nИгрок " + player1.getName() + " делает свой ход.");
-            playerNum = console.nextInt();
-            if(playerNum > randomNum) {
-                System.out.println("Данное число больше того, что загадал компьютер");
-            } else if(playerNum < randomNum) {
-                System.out.println("Данное число меньше того, что загадал компьютер");
-            } else {
-                System.out.println("\nПобедил " + player1.getName());
-                break;
-            }
-
-            System.out.println("\nИгрок " + player2.getName() + " делает свой ход.");
-            playerNum = console.nextInt();
-            if(playerNum > randomNum) {
-                System.out.println("Данное число больше того, что загадал компьютер");
-            } else if(playerNum < randomNum) {
-                System.out.println("Данное число меньше того, что загадал компьютер");
-            } else {
-                System.out.println("\nПобедил " + player1.getName());
-                break;
-            }
+        System.out.println("У вас есть по 10 попыток.");
+        for(i = 0; i <= 10; i++) {
+            checkWinPlayer1();
+            if (isStop) { break; }
+            checkWinPlayer2();
+            if (isStop) { break; }
         }
+        System.out.println("Числа игрока " + player1.getName());
+        player1.printArray1();
+        System.out.println("\nЧисла игрока " + player2.getName());
+        player2.printArray2();
     }
 }
--- src/com/startjava/lesson_2_3_4/game/Player.java
+++ src/com/startjava/lesson_2_3_4/game/Player.java
@@ -1,13 +1,53 @@
 package com.startjava.lesson_2_3_4.game;
 
+import java.util.Arrays;
+
 public class Player {
     private String name;
+    private int[] numbers1 = new int[10];
+    private int[] numbers2 = new int[10];
+
+    public String getName() {
+        return name;
+    }
+
+    public void setNumbers1(int num, int i) {
+        numbers1[i] = num;
+    }
+
+    public void setNumbers2(int num, int i) {
+        numbers2[i] = num;
+    }
 
     public Player(String name) {
         this.name = name;
     }
 
-    public String getName() {
-        return name;
+    public void printArray1() {
+        for (int i = 0; i < numbers1.length; i++) {
+            if (numbers1[i] == 0) {
+                int[] numbers1Copy = Arrays.copyOf(numbers1, i);
+                for (int number: numbers1Copy) {
+                    System.out.print(number + " ");
+                }
+                Arrays.fill(numbers1, 0, i, 0);
+                break;
+            }
+        }
     }
+
+    public void printArray2() {
+        for (int i = 0; i < numbers2.length; i++) {
+            if (numbers2[i] == 0) {
+                int[] numbers2Copy = Arrays.copyOf(numbers2, i);
+                for (int number: numbers2Copy) {
+                    System.out.print(number + " ");
+                }
+                Arrays.fill(numbers2, 0, i, 0);
+                break;
+            }
+        }
+    }
+
+
 }
